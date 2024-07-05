80b1f566f902ea7aceefa19cb2062a976b7ad6a3
Balázs Marcell <KoegLike@proton.me>
2024-07-05 18:46:45 +0300

    debbuged
    didnt do much other

--- src/app/pages/create_user.rs
+++ src/app/pages/create_user.rs
@@ -1,9 +1,16 @@
-use gloo::{ events::EventListener};
-use leptos::{logging::{warn,log}, prelude::*, *};
+use gloo::events::EventListener;
+use js_sys::Math::log;
+use leptos::{
+    logging::{log, warn},
+    prelude::*,
+    *,
+};
 use leptos_router::*;
 use leptos_use::use_permission;
 use tracing::event;
 use web_sys::{DisplayMediaStreamConstraints, MediaDevices, MediaStream};
+
+use crate::components::create;
 
 #[component]
 pub fn CreateUserPage() -> impl IntoView {
@@ -27,41 +34,36 @@ pub fn CreateUserPage() -> impl IntoView {
         })
     };
 
-    if cfg!(any(target_arch = "wasm32", target_arch = "wasm64")) {
-        let lol=create_action(|_: &()| lol());
-        lol.dispatch(());
-    }
+    let (image_url, set_image_url) = create_signal(String::new());
+
+    let lol = create_action(move |_: &()| camera(set_image_url));
+    lol.dispatch(());
 
     view! {
-        <canvas id="canvas" style="display:none;"></canvas>
         <video id="video">"Video stream not available."</video>
         <img id="photo" alt="The screen capture will appear in this box."/>
         <button id="start-button">"Take photo"</button>
     }
 }
 
-async fn lol() {
+async fn camera(image_url: WriteSignal<String>) {
     use wasm_bindgen::prelude::*;
     use wasm_bindgen_futures::JsFuture;
-    let width = 320;
 
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
-    log!("got document and window");
 
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas = canvas
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
-    log!("got canvas");
     let context = canvas
         .get_context("2d")
         .unwrap()
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-    log!("got context");
 
     let camera = window
         .navigator()
@@ -73,50 +75,50 @@ async fn lol() {
                 .audio(&JsValue::from(false)),
         )
         .unwrap();
-    log!("got camera promise");
-    let camera = match JsFuture::from(camera).await{
+    let camera = match JsFuture::from(camera).await {
         Ok(camera) => camera,
         Err(e) => {
             warn!("Error getting camera: {:?}", e);
             return;
         }
-    
     };
-    log!("got camera jsv");
     let camera = camera.dyn_into::<MediaStream>().unwrap();
-    log!("got camera");
 
     let video = document.get_element_by_id("video").unwrap();
     let video = video
         .dyn_into::<web_sys::HtmlVideoElement>()
         .map_err(|_| ())
         .unwrap();
-    log!("got video");
-    video.set_src_object(Some(&camera));
-    log!("set video src object");
 
-    while video.video_width() == 0 {}
+    let video = Box::new(video);
+    let camera = Box::new(camera);
+    video.set_src_object(Some(&camera));
+    let promise = video.play().unwrap();
+    JsFuture::from(promise).await.unwrap();
+    //Box::leak(video);
+    //Box::leak(camera);
 
     let start_button = document.get_element_by_id("start-button").unwrap();
     let start_button = start_button
         .dyn_into::<web_sys::HtmlButtonElement>()
         .map_err(|_| ())
         .unwrap();
-    log!("got start button");
     let event_listener = EventListener::new(&start_button, "click", move |ev| {
-        canvas.set_width(width);
-        let height = video.video_height() / video.video_width() * width;
-        canvas.set_height(height);
+        log!("click");
+        canvas.set_width(video.video_width());
+        canvas.set_height(video.video_height());
         context
             .draw_image_with_html_video_element_and_dw_and_dh(
                 &video,
                 0.0,
                 0.0,
-                width as f64,
-                height as f64,
+                video.video_width() as f64,
+                video.video_height() as f64,
             )
             .unwrap();
         let data_url = canvas.to_data_url_with_type("image/png").unwrap();
+        image_url(data_url.clone());
+        log!("{}", &data_url);
         let photo = document.get_element_by_id("photo").unwrap();
         let photo = photo
             .dyn_into::<web_sys::HtmlImageElement>()
