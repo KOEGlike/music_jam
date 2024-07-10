1476695d59f511f8ee134a9ccfafe3a4ef7f2369
Balázs Marcell <KoegLike@proton.me>
2024-07-10 17:41:15 +0300

    lol

--- src/app/components/host_only/player.rs
+++ src/app/components/host_only/player.rs
@@ -4,19 +4,16 @@ use leptos_use::{use_websocket, UseWebNotificationReturn, UseWebSocketOptions};
 use rust_spotify_web_playback_sdk::prelude as sp;
 
 #[server]
-async fn get_access_token(host_id: &str) -> Result<rspotify::Token, ServerFnError> {
+async fn get_access_token(host_id: String) -> Result<rspotify::Token, ServerFnError> {
     use crate::app::general::*;
     let app_state = expect_context::<AppState>();
     let pool= &app_state.db.pool;
     let id=check_id_type(host_id, pool).await;
-    get_access_token(pool, jam_id)
     Ok(())
 }
 
 #[component]
-pub fn Player(host_id: &str) -> impl IntoView {
+pub fn Player(host_id: String) -> impl IntoView {
     
 
-    view! {
-    }
 }
