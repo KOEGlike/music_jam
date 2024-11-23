10497cd88f108c50d8cc4b2e9e44ffe776930a4c
Balázs Marcell <KoegLike@proton.me>
2024-11-23 17:40:56 +0200

    fixed modals

--- src/components/general/modal.rs
+++ src/components/general/modal.rs
@@ -3,8 +3,19 @@ use leptos::prelude::*;
 #[component]
 pub fn Modal(#[prop(into)] visible: Signal<bool>, children: Children) -> impl IntoView {
     view! {
-        <dialog class="modal" prop:open=visible>
-            {children()}
-        </dialog>
+        <div
+            class="modal-wrapper"
+            style:display=move || {
+                match visible.get() {
+                    true => "",
+                    false => "none",
+                }
+            }
+        >
+
+            <dialog class="modal" prop:open=visible>
+                {children()}
+            </dialog>
+        </div>
     }
 }
